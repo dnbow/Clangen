addd0553573de012b159e8ea3a633f7408abdd1a
dnbow <dnbow@DIMSEYTRON>
2024-01-03 20:49:11 +0000

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,44 @@
+﻿
+
+using SDL2;
+using System;
+using static SDL2.SDL;
+using static SDL2.SDL_image;
+
+namespace Clangen
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            GameState gameState = new GameState();
+
+
+            while (gameState.isRunning)
+            {
+                while (SDL_PollEvent(out SDL_Event e) == 1)
+                {
+                    switch (e.type)
+                    {
+                        case SDL_EventType.SDL_QUIT:
+                            gameState.isRunning = false;
+                            break;
+                    }
+                }
+
+                if (SDL_RenderClear(gameState.Renderer) < 0)
+                    Console.WriteLine($"There was an issue with clearing the render surface. {SDL_GetError()}");
+                
+
+
+                SDL_RenderPresent(gameState.Renderer);
+            }
+
+            SDL_DestroyRenderer(gameState.Renderer);
+            SDL_DestroyWindow(gameState.Window);
+            SDL_Quit();
+
+            return 0;
+        }
+    }
+}
---
+++ Scripts/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static SDL2.SDL;
+using static SDL2.SDL_image;
+
+namespace Clangen
+{
+    public class GameState
+    {
+        public IntPtr Window;
+        public IntPtr Renderer;
+
+        public bool isRunning = false;
+
+        public int Init()
+        {
+            SDL_SetHint(SDL_HINT_WINDOWS_DISABLE_THREAD_NAMING, "1");
+            if (SDL_Init(SDL_INIT_VIDEO) < 0)
+            {
+                Console.WriteLine($"There was an issue initilizing SDL. {SDL_GetError()}");
+                return 10;
+            }
+
+            Window = SDL_CreateWindow("SDL .NET 6 Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WindowFlags.SDL_WINDOW_SHOWN);
+            if (Window == IntPtr.Zero)
+            {
+                Console.WriteLine($"There was an issue creating the window. {SDL_GetError()}");
+                return 11;
+            }
+
+            Renderer = SDL_CreateRenderer(Window, -1, SDL_RendererFlags.SDL_RENDERER_ACCELERATED | SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC);
+            if (Renderer == IntPtr.Zero)
+            {
+                Console.WriteLine($"There was an issue creating the renderer. {SDL_GetError()}");
+                return 12;
+            }
+
+            if (IMG_Init(IMG_InitFlags.IMG_INIT_PNG) == 0)
+            {
+                Console.WriteLine($"There was an issue initilizing SDL2_Image {IMG_GetError()}");
+                return 13;
+            }
+
+            SDL_SetRenderDrawColor(Renderer, 135, 206, 235, 255);
+            return 0;
+        }
+    }
+}
